1a598053ebbd469baec38a6c00a6902d4b388799
isnancahyadi <isnan.arifc@gmail.com>
2024-10-27 18:00:47 +0700

    reset project

--- utils/response.js
+++
@@ -1,9 +0,0 @@
-const response = (statusCode, stat, message, payload, res) => {
-  res.status(statusCode).json({
-    status: stat,
-    message: message,
-    ...(payload && { payload: payload }),
-  });
-};
-
-module.exports = response;
