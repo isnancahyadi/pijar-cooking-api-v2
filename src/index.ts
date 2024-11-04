fd89786784d89e81b96780b111f93f09fa29de15
isnancahyadi <isnan.arifc@gmail.com>
2024-11-04 22:00:07 +0700

    [FIX] fixing structure code

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,15 @@
-import * as dotenv from 'dotenv';
 import express, { Application } from 'express';
-import mysql, { PoolOptions } from 'mysql2';
 
 import appMiddleware from './middleware/App';
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+import MainRouter from './routes';
 
-dotenv.config();
 const app: Application = express();
 const port: number =
   process.env.PORT != null ? parseInt(process.env.PORT) : 4000;
 
-const dbPool: PoolOptions = {
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'pijar_cooking',
-};
-
-const dbConnect = mysql.createPool(dbPool);
-
 app.use(appMiddleware);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/recipes', (req, res) => {
-  dbConnect.execute('SELECT * FROM recipes', (err, rows) => {
-    if (err) {
-      res.json({
-        message: 'connection failed!',
-      });
-    }
-    res.json({
-      message: 'connection success',
-      data: rows,
-    });
-  });
-});
+app.use('/api', MainRouter);
 
 app.listen(port, () => {
   console.log(`Listening app on http://localhost:${port}`);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import RecipeRouter from './recipe';
 const router = Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.send('Hello, API is running well. ^_^');
 });
+
+router.use('/recipe', RecipeRouter);
 
 export default router;
